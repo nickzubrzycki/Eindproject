6f23dc167c2774eaee1a352fbcfc1af840291e4d
Arno Slabbinck <arno.slabbinck@hotmail.com>
2021-08-09 13:52:42 +0200

    Aangemaakt entityObjecten voor database

---
+++ Eindproject/Domain/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eindproject.Domain
+{
+    public class Status
+    {
+        [Key]
+        public int StatusId { get; set; }
+
+        // Omschrijving: Als film/Serie gepland, gekeken of aan het kijken is
+        public string Omschrijving { get; set; }
+
+    }
+}
